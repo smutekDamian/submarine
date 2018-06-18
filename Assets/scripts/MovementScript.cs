3c4f5e4abaf67be53e6c5bb8fad67e17fe49c393
smutekDamian <smutek.damian95@gmail.com>
2018-06-18 20:45:32 +0200

    change accel to keyboard

---
+++ Assets/scripts/MovementScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public Rigidbody submarine;
+
+    public void Start()
+    {
+    }
+
+    public void Update()
+    {
+
+        if (Input.GetKey("space"))
+        {
+            transform.position -= transform.forward * Time.deltaTime * 30;
+            if (Input.GetKey("right"))
+            {
+                transform.Rotate(0, Time.deltaTime * 5 * 50, 0);
+            }
+
+            if (Input.GetKey("left"))
+            {
+                transform.Rotate(0, -(Time.deltaTime * 5 * 50), 0);
+            }
+
+            if (Input.GetKey("up"))
+            {
+                transform.Rotate(Time.deltaTime * 5 * 50, 0, 0);
+            }
+
+            if (Input.GetKey("down"))
+            {
+                transform.Rotate(-(Time.deltaTime * 5 * 50), 0, 0);
+            }
+        }
+    }
+}
