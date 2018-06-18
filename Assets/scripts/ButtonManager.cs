6304d0808983a53ed13c546a3de7e69e9577aaa4
smutekDamian <smutek.damian95@gmail.com>
2018-06-18 19:34:04 +0200

    init

---
+++ Assets/scripts/ButtonManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+    public void NewGameBtn(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Text scoreText;
+    public Text winText;
+    private int count;
+    
+    void Start()
+    {
+        count = 0;
+        winText.text = "";
+        SetScoreText();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetScoreText();
+            if (count == 6)
+            {
+                winText.text = "You Won !";
+            }
+        }
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + count.ToString();
+    }
+}
